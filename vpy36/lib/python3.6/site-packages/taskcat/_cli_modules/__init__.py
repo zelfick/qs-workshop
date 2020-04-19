5c4e720ec9110406e091384a0b1a39a61a2f8da7
jherran <jorge.herran@endava.com>
2020-04-18 23:05:52 -0500

    Test Cat

--- vpy36/lib/python3.6/site-packages/taskcat/_cli_modules/__init__.py
+++
@@ -1,7 +0,0 @@
-from .delete import Delete  # noqa: F401
-from .deploy import Deploy  # noqa: F401
-from .lint import Lint  # noqa: F401
-from .list import List  # noqa: F401
-from .package import Package  # noqa: F401
-from .test import Test  # noqa: F401
-from .update_ami import UpdateAMI  # noqa: F401
